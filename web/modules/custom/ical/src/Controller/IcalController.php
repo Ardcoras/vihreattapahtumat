a647b864a3d07b1ac3c1a78b51faa21afcc6e656
Jyri-Petteri Paloposki <jyri-petteri.paloposki@iki.fi>
2025-11-24 02:14:19 +0200

    Use custom iCal, the module doesn't work too well

---
+++ web/modules/custom/ical/src/Controller/IcalController.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\ical\Controller;
+
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Returns ical.
+ */
+class IcalController implements ContainerInjectionInterface {
+
+  /**
+   * The controller constructor.
+   */
+  public function __construct(
+    private readonly EntityTypeManagerInterface $entityTypeManager,
+  ) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container): self {
+    return new self(
+      $container->get('entity_type.manager'),
+    );
+  }
+
+  public function getIcal(Request $request): Response {
+    $organisers = (int) $request->query->get('organiser');
+
+    $storage = $this->entityTypeManager
+      ->getStorage('node');
+    $query = $storage
+      ->getQuery()
+      ->condition('type', 'event')
+      ->condition('field_event_organiser', $organisers)
+      ->accessCheck(FALSE);
+    $res = $query->execute();
+
+    $nodes = $storage->loadMultiple($res);
+    $events = [];
+    foreach ($nodes as $node) {
+      $uuid = new \Eluceo\iCal\Domain\ValueObject\UniqueIdentifier($node->uuid->getString());
+      $event = new \Eluceo\iCal\Domain\Entity\Event($uuid);
+      $event->addCategory(new \Eluceo\iCal\Domain\ValueObject\Category($node->field_event_type->entity->getName()));
+      $event->setSummary($node->getTitle());
+      $event->setDescription(strip_tags((string) $node->field_description->getValue()[0]['value']));
+
+      $location = $node->field_place->entity;
+      $location_parts = [
+        $location->getName(),
+        (string) $location->field_street_address->getString(),
+        (string) $location->field_address_city->getString(),
+      ];
+      $event->setLocation(new \Eluceo\iCal\Domain\ValueObject\Location(implode(', ', $location_parts)));
+      $event->setUrl(new \Eluceo\iCal\Domain\ValueObject\Uri($node->toUrl('canonical', ['absolute' => TRUE])->toString()));
+      $event->setOccurrence(
+        new \Eluceo\iCal\Domain\ValueObject\TimeSpan(
+          new \Eluceo\iCal\Domain\ValueObject\DateTime(
+            \DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s', $node->field_event_datetime->getValue()[0]['value']), FALSE
+          ),
+          new \Eluceo\iCal\Domain\ValueObject\DateTime(
+            \DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s', $node->field_event_datetime->getValue()[0]['end_value']), FALSE
+          )
+        )
+      );
+      $events[] = $event;
+    }
+    
+    $calendar = new \Eluceo\iCal\Domain\Entity\Calendar($events);
+
+    $component_factory = new \Eluceo\iCal\Presentation\Factory\CalendarFactory();
+    $component = $component_factory->createCalendar($calendar);
+
+    $response = new Response((string) $component, 200, ['Content-Type: text/calendar; charset=utf-8']);
+    return $response;
+  }
+
+}
